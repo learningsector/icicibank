5ddfc8f468248101c72c999b0adf1bd5178467ee
bhaskarmk <bhaskarmkul1098@gmail.com>
2022-12-02 04:02:03 +0530

    updated

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 
-hi i am updating this
+
+hi i am updating thi
+cherry-pick demoo strats
+this is for deploymient files information....
+this is the file is used fro the 
+
 this file having some deployment logic file
