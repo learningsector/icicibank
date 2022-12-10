cdcdd12aa4a9a49194491293d922d84fd7003e65
bhaskarmk <bhaskarmkul1098@gmail.com>
2022-12-10 23:49:55 +0530

    first commit_10th dec

---
+++ movie.sh
@@ -0,0 +1,8 @@
+echo "this is file for the creation of movies stories"
+read -p "enter your movie name:" name
+if [ name -eq  0 ]
+then 
+	echo "this is in your fule system"
+else
+	echo "this is not your movie "
+fi
